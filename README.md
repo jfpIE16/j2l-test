85ffbdd87a974e89e9aeeb7c413af00c85c2a865
Fernando <jf23perez@gmail.com>
2025-01-21 08:57:18 -0600

    Add documentation

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# j2l-test
-Prueba tecnica j2l
+# Prueba tecnica j2l
+
+## ETL
+EL script *etl.py* tiene la siguiente estructura
+* extract()
+* transform()
+* load()
+
+### extract
+```python
+def extract(filepath) -> pd.DataFrame:
+    logging.info(f"Extracting data from {filepath}")
+    return pd.read_json(filepath)
+```
+Se carga el contenido del archivo *data/test_tecnico.json* a un dataframe de pandas.
+
+### transform
+```python
+def transform(df: pd.DataFrame) -> pd.DataFrame:
+    transfomed_df = (
+        df.pipe(string_normalization, schema=SCHEMA)
+        .pipe(coalesce_cols)
+        .pipe(change_column_name, schema_change=NAME_CHANGE)
+        .pipe(handle_blanks, schema=SCHEMA)
+        .pipe(format_date, schema=SCHEMA)
+        .pipe(calculations, calculations=CALCULATIONS)
+    )
+    logging.info("Transforming vehicle data.")
+    return transfomed_df
+```
+Utilizando la característica *pipe* de pandas se aplican las transformaciones de forma secuencial en el conjunto de datos. Ver la documentación de las transformaciones en [transforms.py](transforms.py)
+
+### load
+```python
+def load(df: pd.DataFrame) -> bool:
+    con = DatabaseConnection()
+    with con.get_connection() as connection:
+        rowcount = df.to_sql('vehiculos', con=connection, dtype=DB_SCHEMA, index=False, if_exists='replace')
+        logging.info(f"Succesfully loaded {rowcount} rows.")
+```
+Se crea una conexión de tipo *DatabaseConnection* (se utilizo una clase con el objetivo de simplificar el código, en el futuro se podrían manejar cursores de forma fácil usando la clase), y se insertan los datos a la BD. Se utiliza la condición *replace* para no crear duplicidad en los datos.
+
+### Resultados
+A continuación se muestra la tabla final con las transformaciones requeridas.
+![Resultados](assets/output.png)
--- transforms.py
+++ transforms.py
@@ -1,18 +1,45 @@
 import pandas as pd
 
 def string_normalization(df: pd.DataFrame, schema: dict = {}):
+    """
+    String transformation.
+    Args:
+        df -> Pandas DataFrame
+        schema -> Dictionary with schema information.
+
+    Return:
+        df -> Pandas DataFrame with transformed columns.
+    """
     for col, type in schema.items():
         if type == 'str':
             df[col] = df[col].str.upper()
     return df
 
 def change_column_name(df: pd.DataFrame, schema_change: dict = {}):
+    """
+    Column name
+    Args:
+        df -> Pandas DataFrame
+        schema_change -> Dictionary with columns to rename.
+
+    Return:
+        df -> Pandas DataFrame with renamed columns.
+    """
     for key, val in schema_change.items():
         df[val] = df[key]
         df.drop([key], axis=1, inplace=True)
     return df
 
 def handle_blanks(df: pd.DataFrame, schema: dict = {}):
+    """
+    Blanks handling.
+    Args:
+        df -> Pandas DataFrame
+        schema -> Dictionary with schema information.
+    Usage: If the column in schema is of type int, then it will replace NULLS with 0.
+    Return:
+        df -> Pandas DataFrame with transformed columns.
+    """
     for key, type in schema.items():
         if type == 'int':
             df[key] = df[key].fillna(0).astype(int)
@@ -20,17 +47,44 @@ def handle_blanks(df: pd.DataFrame, schema: dict = {}):
     return df
 
 def coalesce_cols(df: pd.DataFrame):
+    """
+    Blanks handling.
+    Args:
+        df -> Pandas DataFrame
+    Usage: Coalesce columns costo_vehiculo, costo_veiculo maybe a typo in the creation of the JSON document.
+    Return:
+        df -> Pandas DataFrame with transformed columns.
+    """
     df['costo_vehiculo'] = df[['costo_vehiculo','costo_veiculo']].bfill(axis=1).iloc[:, 0]
     df.drop(['costo_veiculo'], axis=1, inplace=True)
     return df
 
 def format_date(df: pd.DataFrame, schema: dict = {}):
+    """
+    Date formatting.
+    Args:
+        df -> Pandas DataFrame
+        schema -> Dictionary with schema information.
+    Usage: If the column in schema is of type date, then it will formate the date as dd-mm-YYYY.
+    Return:
+        df -> Pandas DataFrame with transformed columns.
+    """
     for col, type in schema.items():
         if type == 'date':
             df[col] = pd.to_datetime(df[col]).dt.strftime("%d-%m-%Y")
     return df
 
 def calculations(df: pd.DataFrame, calculations: dict = {}):
+    """
+    Metrics and calculations
+    Args:
+        df -> Pandas DataFrame
+        calculations -> Dictionary with new column and definition.
+    Usage: You can run calculations that involve multiple columns in pandas.
+    Example: 'margen': 'precio_venta - costo_vehiculo' 
+    Return:
+        df -> Pandas DataFrame with transformed columns.
+    """
     for col_name, calculation in calculations.items():
         df[col_name] = df.eval(f"{calculation}")
     return df
